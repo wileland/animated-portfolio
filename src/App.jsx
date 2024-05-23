dc0b2900aeb85d720137e6bba5667c0361faf802
wileland <wileland7@gmail.com>
2024-05-23 04:59:02 -0500

    Updating Services component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
@@ -11,14 +12,14 @@ const App = () => {
       <Hero />
     </section>
     <section id="Services"><Parallax type="services" /></section>
-    <section>Services</section>
+    <section><Services/></section>
     <section id="Portfolio"><Parallax type="portfolio" /></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
     <section id="Contact">Contact</section>
-    {/* <Test/> */}
-    <Test />
+    {/* <Test/>
+    <Test /> */}
   </div>;
 };
 
