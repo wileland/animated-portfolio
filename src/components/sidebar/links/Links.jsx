d6609259bddd1844a368d733489188df2659a249
wileland <wileland7@gmail.com>
2024-05-22 22:20:49 -0500

    Updated Links component- fixed stagger animation

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,31 +1,43 @@
+import { motion } from "framer-motion";
+
+
 const variants = {
-  open:{
-    transition:{
-      staggeredChildren: 0.1
-    }
+  open: {
+    transition: {
+      staggerChildren: 0.1
+    },
   },
   closed:{
     transition:{
-      staggeredChildren: 0.05,
-      staggeredDirection: -1,
-    }  
-  }
-}
-
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },  
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity:1, 
+  },
+  closed: {
+    y: 50,
+    opacity:0, 
+  },
+};
 const Links = () => {
   const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
 
     
 
     return (
-      <div className="links">
+      <motion.div className="links" variants={variants}>
         {items.map((item) => (
-          <a href={`#${item}`} key={item}>
+          <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{scale:1.1}}>
             {item}
-          </a>
+          </motion.a>
         ))}
-      </div>
+      </motion.div>
     );
-  };
+};
 
   export default Links;
