d13f3e7983f9e88ff621d57f51242076c8201967
wileland <wileland7@gmail.com>
2024-05-23 09:15:39 -0500

    Created portfolio component, style and updated app

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,34 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio"; // Ensure this import is correct
 
 const App = () => {
-  return <div>
-    <section id="Homepage">
-      <Navbar />
-      <Hero />
-    </section>
-    <section id="Services"><Parallax type="services" /></section>
-    <section><Services/></section>
-    <section id="Portfolio"><Parallax type="portfolio" /></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="Contact">Contact</section>
-    {/* <Test/>
-    <Test /> */}
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section>
+        <Portfolio /> {/* Make sure Portfolio is properly used here */}
+      </section>
+      
+      <section id="Contact">Contact</section>
+      {/* <Test/>
+      <Test /> */}
+    </div>
+  );
 };
 
 export default App;
+
