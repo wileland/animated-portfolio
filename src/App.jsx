6fcc3f64edd7d03befe202cdef5804e92225801e
wileland <wileland7@gmail.com>
2024-05-23 03:58:57 -0500

    Updated parallax images x & y

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import Test from "./Test";
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return <div>
-      <section id="Homepage">
-        <Navbar/>
-        <Hero/>
-      </section>
-      <section id="Services">Parallax</section>
-      <section>Services</section>
-      <section id="Portfolio">Parallax</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section id="Contact">Contact</section>
+    <section id="Homepage">
+      <Navbar />
+      <Hero />
+    </section>
+    <section id="Services"><Parallax type="services" /></section>
+    <section>Services</section>
+    <section id="Portfolio"><Parallax type="portfolio" /></section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+    {/* <Test/> */}
+    <Test />
   </div>;
-
 };
 
 export default App;
