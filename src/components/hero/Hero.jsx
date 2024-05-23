e4b6d7d9940de4669cb26bb0e8ad0f4d6dd8cbb2
wileland <wileland7@gmail.com>
2024-05-23 00:01:32 -0500

    Updated Hero component and scss

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,20 +3,23 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
-       <div className="wrapper">
-          <div className="textContainer">
-             <h2>William L. Haynes</h2>
-             <h1>Web developer and UI designer</h1>
-             <div className="buttons">
-                 <button>See the Latest Works</button>
-                 <button>Contact Me</button>
-                </div>
-                <img src="/scroll.png" alt="" />
-            </div>
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>WILLIAM L. HAYNES</h2>
+          <h1>Web developer and UI designer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
         </div>
-        <div className="imageContainer">
-            <img src="/hero.png" alt="" />
-        </div>
+      </div>
+      <div className="slidingTextContainer">
+        Writer Content Creator Influencer
+      </div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
     </div>
   );
 };
