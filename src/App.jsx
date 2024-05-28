ff3aa798c1c2414ae66e2b002bc37d0c5397e1a4
wileland <wileland7@gmail.com>
2024-05-27 19:27:26 -0500

    cursor fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
@@ -29,6 +31,8 @@ const App = () => {
       <section id="Contact">
         <Contact />
       </section>
+      {/* <Test /> */}
+      {/* <Test /> */}
     </div>
   );
 };
