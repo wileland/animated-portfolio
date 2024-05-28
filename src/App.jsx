34c7e8b176ab5f6bb3c1df3a30f7a36a66175f50
wileland <wileland7@gmail.com>
2024-05-28 02:51:20 -0500

    updated app(2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Test from "./Test";
 import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
-import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
-import Cursor from "./components/cursor/Cursor";
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
@@ -29,6 +29,9 @@ const App = () => {
       <section id="Contact">
         <Contact />
       </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
     </div>
   );
 };
