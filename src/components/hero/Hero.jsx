2de9c05f8fa8ae741efc124c952a85234c564317
wileland <wileland7@gmail.com>
2024-05-28 00:55:51 -0500

    pre build

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -33,7 +33,7 @@ const sliderVariants = {
     x: "-220%",
     transition: {
       repeat: Infinity,
-      repeatType: "mirror",
+      repeatType:"mirror",
       duration: 20,
     },
   },
@@ -50,9 +50,7 @@ const Hero = () => {
           animate="animate"
         >
           <motion.h2 variants={textVariants}>WILLIAM L. HAYNES</motion.h2>
-          <motion.h1 variants={textVariants}>
-            Web developer and UI designer
-          </motion.h1>
+          <motion.h1 variants={textVariants}>Web developer and UI designer</motion.h1>
           <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants} className="hover-pointer">
               See the Latest Works
@@ -64,8 +62,7 @@ const Hero = () => {
           <motion.img
             variants={scrollButtonVariants}
             src="/scroll.png"
-            alt="Scroll"
-            className="scrollImage"
+            alt=""
           />
         </motion.div>
       </div>
@@ -78,7 +75,7 @@ const Hero = () => {
         Writer Content Creator Influencer
       </motion.div>
       <div className="imageContainer">
-        <img src="/hero.png" alt="Hero" />
+        <img src="/hero.png" alt="" />
       </div>
     </div>
   );
